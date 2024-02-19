48bbb1b651839ec1f6a80a3af29ac913778f441e
namanshah063 <101341906+namanshah063@users.noreply.github.com>
2024-02-19 18:31:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// function add_animation(class){
+//   var element = document.getElementByClassName();
+// element.classList.add("otherclass");
+// }
+
+document.querySelector(".invite").classList.add("main");
+document.querySelector(".invite-text").classList.add("typed");
+ function f1(){
+   document.querySelector(".cursive").classList.add("main");
+   document.querySelector(".cursive-text").classList.add("typed");
+ }
+ function f2(){
+   document.querySelector(".ceremony").classList.add("main");
+   document.querySelector(".ceremony").classList.add("width");
+   document.querySelector(".ceremony-text").classList.add("typed");
+ }
+
+ setTimeout(f1,1500);
+ setTimeout(f2,3000);
+ setTimeout(f3,4500);
